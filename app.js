b2a86b894c30823e01737bf2fdeda49c3f2921ff
stephanie angibert <stephanieangibert@orange.fr>
2020-10-22 19:32:01 +0200

    ca doit etre le 5 commit

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ let video=document.getElementById("video1");
 console.log(video);
 let play =document.querySelector(".play");
 let i=document.querySelectorAll("i")[1];
+let tousLesI=document.querySelectorAll("i");
 console.log(i);
+let panneauDeux=document.getElementById("planDeux");
 let playing=true;
 let pp=document.querySelector(".playPause");
 pp.innerHTML="Play ";
 let yeuxVert=document.getElementById("yeuxVert");
+let lampe=document.getElementById("lampe");
+lampe.addEventListener("click",()=> {
+    panneauDeux.classList.toggle("couleur");
+    console.log("coucou");
+})
 
 
 console.log(play);
